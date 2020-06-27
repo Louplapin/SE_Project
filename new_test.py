b914d725cce8303d2d35b578d8b2b5f23197b240
dabi621 <60123541+dabi621@users.noreply.github.com>
2020-06-27 16:31:48 +0900

    add creat map

--- new_test.py
+++ new_test.py
@@ -3,18 +3,36 @@ txt = File_data.read()
 
 tree=txt.split('nodes:')[0]
 treelist = list(tree.split('\n'))
-print(treelist)
 
-
-
+#creat nodelist and map
+#init map
+nodemap = {}
+#cut string
 nodes= (txt.split('nodes:')[1]).split('branches:')[0]
+#creat list
 nodeslist = list(nodes.split('\n'))
+#delete space
 nodeslist.remove('')
-print(nodeslist)
+nodeslist.remove('')
+#creat map
+for index,char in enumerate(nodeslist):
+    name = char.split()
+    nodemap.update({index:name[1]})
+#output map example
+print(nodemap[1])
 
-
+branchemap = {}
 branches=txt.split('branches:')[1]
 brancheslist = list(branches.split('\n'))
-print(brancheslist)
-#print(tree , nodes, branches)
+brancheslist.remove('')
+for char in brancheslist:
+    name = char.split(', ')
+    key = name[0]
+    value = name[1]
+    if key not in branchemap:
+        branchemap[key] = []
+    branchemap[key].append(value)
+print(branchemap)
+
+    #branchemap.update({index:name[1]})
 
