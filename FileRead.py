9c317f569be0d7949860acff30e6793e3e5e4f43
Louplapin <loup0620@gmial.com>
2020-06-27 14:31:28 +0900

    fix FireRead.py

--- FileRead.py
+++ FileRead.py
@@ -1,7 +1,9 @@
 import sys
 
-f = open(sys.argv[1], 'r')
-list = f.readlines()
-for line in list:
-    print(line)
-f.close()
+File_data = open(sys.argv[1], "r", encoding='utf-8')
+txt = File_data.read()
+
+tree=txt.split('nodes:')[0]
+nodes= (txt.split('nodes:')[1]).split('branches:')[0]
+branches=txt.split('branches:')[1]
+print(tree , nodes, branches)
