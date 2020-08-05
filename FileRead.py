a90c92ddd76f315c61cbc9a46fd17a670d9d24e3
Louplapin <loup0620@gmial.com>
2020-08-05 21:02:13 +0900

    delete

--- FileRead.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-File_data = open(sys.argv[1], "r", encoding='utf-8')
-txt = File_data.read()
-
-tree = txt.split('nodes:')[0]
-nodes = (txt.split('nodes:')[1]).split('branches:')[0]
-branches = txt.split('branches:')[1]
-print(tree , nodes, branches)
